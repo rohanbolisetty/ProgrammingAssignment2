368ff126a0ca4d1dc1118d9c1f8b7095c8219139
rohanbolisetty <rohan.bolisetty@gmail.com>
2017-09-10 04:15:31 +0530

    Attempt 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,13 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+# The makeCacheMatrix gives the input matrix 4 functions to help in the cache process
+# The cacheSolve returns the inverse if already computed else it computes it and sets
+# the inverse so that it need not be computed again and directly be used
 
 ## Write a short comment describing this function
+# This function takes in the matrix and assigns it 4 values in a way that if the
+#inverse already exists, it is registered in the getinv value of the matrix
+# you can also change the matrix directly using the set function
 
 makeCacheMatrix <- function(x = matrix()) {
   m<-NULL
@@ -17,7 +23,9 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-
+# This function checks if the inverse is already computed and if yes , returns a 
+#message saying getting cached data and gives the inverse, otherwise is computes
+# the inverse and sets the setinv value of the matrix to the inverse
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   m<-x$getinv()
